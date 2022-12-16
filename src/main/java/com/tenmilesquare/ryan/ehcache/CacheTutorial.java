33d329ee5af8351330e4dd1cc6c6bd25222701b7
Ryan Van Fleet <ryan@tenmilesquare.com>
2022-12-16 13:52:38 -0500

    Added statistics

--- src/main/java/com/tenmilesquare/ryan/ehcache/CacheTutorial.java
+++ src/main/java/com/tenmilesquare/ryan/ehcache/CacheTutorial.java
@@ -7,15 +7,21 @@ import org.ehcache.config.builders.CacheConfigurationBuilder;
 import org.ehcache.config.builders.CacheManagerBuilder;
 import org.ehcache.config.builders.ResourcePoolsBuilder;
 import org.ehcache.config.units.MemoryUnit;
+import org.ehcache.core.internal.statistics.DefaultStatisticsService;
+import org.ehcache.core.spi.service.StatisticsService;
+import org.ehcache.core.statistics.CacheStatistics;
 
 public class CacheTutorial {
 
     private final static String CACHE_NAME = "musicCache";
 
     public static void main(String[] args) throws CachePersistenceException {
+
+        StatisticsService statisticsService = new DefaultStatisticsService();
         // The cache manager is closable resource. Clean it up after we are done
         try (PersistentCacheManager cacheManager = CacheManagerBuilder.newCacheManagerBuilder()
                 .with(CacheManagerBuilder.persistence("/tmp/ehcache-data"))
+                .using(statisticsService)
                 .withCache(CACHE_NAME,
                         CacheConfigurationBuilder.newCacheConfigurationBuilder(String.class,
                                 Music.class, ResourcePoolsBuilder
@@ -44,7 +50,38 @@ public class CacheTutorial {
             System.out.println("Logic equal 5: " + music5Cache.toString().equals(music5.toString()));
 
             musicCache.forEach(entry -> System.out.println(entry.getValue().toString()));
+            printStats( statisticsService.getCacheStatistics(CACHE_NAME));
 
         }
+    }
+
+    private static void printStats(CacheStatistics cacheStatistics) {
+
+        System.out.println("=========================================");
+        System.out.println("Cache gets: " + cacheStatistics.getCacheGets());
+        System.out.println("Cache puts: " + cacheStatistics.getCachePuts());
+        System.out.println("Cache hits: " + cacheStatistics.getCacheHits());
+        System.out.println("Cache misses: " + cacheStatistics.getCacheMisses());
+        System.out.println("Cache removal: " + cacheStatistics.getCacheRemovals());
+        System.out.println("Cache expiration: " + cacheStatistics.getCacheExpirations());
+        System.out.println("Cache evictions: " + cacheStatistics.getCacheEvictions());
+        System.out.println("Cache hit percentage: " + cacheStatistics.getCacheHitPercentage());
+        System.out.println("Cache miss percentage: " + cacheStatistics.getCacheMissPercentage());
+        cacheStatistics.getTierStatistics().forEach((tierName, tierStats) -> {
+            System.out.println("------------------");
+            System.out.println("Tier: " + tierName);
+            System.out.println("Allocation by size: " + tierStats.getAllocatedByteSize());
+            System.out.println("Occupied size: " + tierStats.getOccupiedByteSize());
+            System.out.println("Evictions: " + tierStats.getEvictions());
+            System.out.println("Expiration: " + tierStats.getExpirations());
+            System.out.println("Hits: " + tierStats.getHits());
+            System.out.println("Misses: " + tierStats.getMisses());
+            System.out.println("Mappings: " + tierStats.getMappings());
+            System.out.println("Puts: " + tierStats.getPuts());
+            System.out.println("Removals: " + tierStats.getRemovals());
+        });
+
+        System.out.println("=========================================");
+
     }
 }
